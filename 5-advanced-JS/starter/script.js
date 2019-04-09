469e609691b475b5ae3ffc3ce6293acfe38e708d
Andrew Ormond <drewthedev9@gmail.com>
2019-04-08 23:33:52 -0400

    primitives vs objects finished

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -42,7 +42,7 @@ console.log(mark.lastName);
 */
 
 // Onject.create method. Another way for inheritance.
-var personProto = {
+/*var personProto = {
     calculateAge: function (){
         console.log (2019 - yearOfBirth);
     }
@@ -61,3 +61,46 @@ var jane = Object.create(personProto, {
     yearOfBirth: {value:1969},
     job: {value: 'designer'}
 })
+*/
+
+// primitives vs objects.
+//  undefined , null , boolean , string and number .
+//  Everything else is an object.
+
+// primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects 
+var obj1 = {
+    name:'john',
+    age: 26
+}
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age)
+console.log(obj2.age)
+
+// Functions
+
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'lisbon'
+}
+
+function change(a,b){
+    a=38;
+    b.city ='san fransisco';
+
+} 
+
+console.log(age); //27
+console.log(obj.city); //san fransisco
+//  the var age valuw wont change but the obj.city proprty will,
+// it only makes areference to the object ist creating a new one.
+// check in lesson 64 - Primitives and objects. 3rd book mark
