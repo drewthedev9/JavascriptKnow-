60eaf8f0220f6e2e43ba74166b2f41fa8c378eee
Andrew Ormond <drewthedev9@gmail.com>
2019-04-10 21:40:01 -0400

    call back functions one

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -67,7 +67,7 @@ var jane = Object.create(personProto, {
 //  undefined , null , boolean , string and number .
 //  Everything else is an object.
 
-// primitives
+/*// primitives
 var a = 23;
 var b = a;
 a = 46;
@@ -104,3 +104,53 @@ console.log(obj.city); //san fransisco
 //  the var age valuw wont change but the obj.city proprty will,
 // it only makes areference to the object ist creating a new one.
 // check in lesson 64 - Primitives and objects. 3rd book mark
+*/
+
+// functions. ->// an array with calcuated ages
+
+var years =[1990 , 1965, 1937, 2005, 1998];
+
+// acts liek a machine to chun out the age results.
+function arrayCalc (arr, fn){
+    //  epmpty array to loop through.
+    var arrRes = [];
+    // starts from 0: as long as i is less than arr.lenghth: increment ++ every loop.
+    for ( var i = 0; i < arr.length;
+        i++) {
+            // inserts an element at the end of the array
+            // fn() uses the function below as a callback and claculates whats being
+            // pushed onto the new arrRes array. PRETTY COOL!
+            arrRes.push(fn(arr[i]));
+        }
+        // returns that aray with all the calculated ages.
+        return arrRes;
+}
+
+// call back function fn -> meaning we will call teh function into teh function above 
+// later. in the arrRes.push(fnarr[i]).
+function calculateAge(el){
+    return 2019 -el;
+}
+
+const isFulllAge=(el)=>{
+    // are ages older than 18 
+    return el >= 18;
+}
+
+// max heart rate function 
+const maxHeartRate =()=> {
+    // rounds to the closest integer.
+    // numbers are the max heart rate formula
+    return Math.round (206.9 - (0.67 * el));
+}
+
+// uses calculateAge as a call back function for the fn() function in arrayCalc().
+var ages = arrayCalc(years, calculateAge);
+
+// function return an array if ages are older than 18. COOL!
+var fullAges = arrayCalc(ages,isFulllAge);
+
+
+
+console.log(ages);
+console.log(fullAges);
