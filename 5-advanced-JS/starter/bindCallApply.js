b80aeb6077a05359f5f41367258b825acb59f262
Andrew Ormond <drewthedev9@gmail.com>
2019-05-17 22:08:31 -0400

    added call back function

--- 5-advanced-JS/starter/bindCallApply.js
+++ 5-advanced-JS/starter/bindCallApply.js
@@ -4,15 +4,32 @@ var john = {
     name: 'john',
     age: 26,
     job: 'teacher',
-    // methofd for joh to present stuff.
+    // methofd for john to present stuff.
     presentation : function(style,timeOfDay){
         if (style === 'formal'){
-            console.log('good' + timeOfDay + 'ladies and gentle men! I\'m',
-             this.name + ', I\'m a' + 
-             this.job + ' and I\'m' +
-             this.age );
+            console.log('good '  + timeOfDay + ' ladies and gentle men! I\'m',
+             this.name + ', I\'m a ' + 
+             this.job + ' and I\'m ' +
+             this.age  + 'years old');
         } else if (style === 'friendly'){
-            console.log()
+            console.log( 'Hey! whats\'s up? I\'m ' + 'I\'m a ' + 
+            this.job + ' and I\'m ' +
+            this.age  + ' years old. Have a nice ' + timeOfDay 
+            );
         }
     }
 }
+
+var emily = {
+    name: 'emily',
+    age: 35,
+    job : 'designer'
+};
+
+//  call function john variable,presentaion method
+
+john.presentation ('formal', 'morning');
+
+// call back method on the function above.
+
+john.presentation.call(emily, 'friendly', 'aftrernoon');
