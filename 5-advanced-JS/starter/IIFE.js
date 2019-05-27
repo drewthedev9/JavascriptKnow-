081d04b97ffcf8cdca1a187a8f36ea4762c88c09
Andrew Ormond <drewthedev9@gmail.com>
2019-05-27 17:23:28 -0400

    IIFEE example

--- 5-advanced-JS/starter/IIFE.js
+++ 5-advanced-JS/starter/IIFE.js
@@ -18,3 +18,32 @@
     console.log(score >= 5 -
     goodLuck);
 }) (5);
+
+// great IIFE example. 
+// section 6 implementing teh module.
+
+// immediatley invoked function expression, 
+// close off functins from public.
+var budgetController = (function(){
+
+    x = 23;
+    // add is private and wont be able to be called from
+    // console.
+    // for example using in console budgetController.add(a) wont work
+    // its private.
+    const add =(a)=>{
+     x + a;
+    }
+    
+    // Public test however will be
+    // calls add function
+    // budgetController.pusblicTest(b) will work its not
+    // private buy the IIFE.
+    return {
+        publicTest: function(b){
+            console.log(add(b));
+        }
+    }
+    
+    })();
+    
