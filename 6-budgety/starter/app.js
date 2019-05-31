2c44011d8571648723dba6ecffd533c9f64d2843
Andrew Ormond <drewthedev9@gmail.com>
2019-05-31 17:04:25 -0400

    added more to DOM manipulation

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,41 +1,72 @@
-// immediatley invoked function expression
+// Budget controller;
 var budgetController = (function(){
 
-x = 23;
 
-const add =(a)=>{
- return x + a;
-}
-
-return {
-    publicTest: function(b){
-        return add(b);
-    }
-}
 
 })();
 
-// es6 function with IIFE. These controllers dont
-//  know about eachother. SEPERATION of concerns.
+// UI controller.
 const UIController = (() =>{
 
-// some code
+    // object to centralize changing the classtypes,
+    // we do it here in the object instead of everywhere in the
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+
+    };
+
+return {
+
+   
+     getInput: function() {
+         return {
+             type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp\
+            //  gets the value of of the input html elemnet.
+             description:document.querySelector(DOMstrings.inputDescription).value,
+             value :document.querySelector(DOMstrings.inputValue).value
+         };
+    },
+    // expose DOMstrings to the public.
+    getDOMstrings: function(){
+        return DOMstrings;
+    }
+ }
 
 })();
 
+// GLOBAL APP controller /w IIFE.
+var controller = (function(budgetCtrl, UIctrl){
 
-const controller = ((budgetCtrl, UIctrl) =>{
+    // can now access the DOM strings
+var DOM = UIctrl.getDOMstrings();
 
-    // this module can now use the other modules code.
-    // but it is still private.
-   var z = budgetCtrl.publicTest(5);
+// To avoid dry.
+var ctrlAddItem = function(){
+    // 1. get the filed input data
+    var input = UIctrl.getInput();
+    console.log(input);
 
-//    create another public method.
-return {
-    anotherPublic: function(){
-        console.log(z);
+    // 2. add the item to the budget calculator
+
+    // 3. add the item to the UI
+
+    // 4. calculate the budget.
+
+    // 5. Display teh budget on th UI.
+
+};
+    
+document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    
+document.addEventListener('keypress', function(event){
+    // keycode for the enter button, which is for older browsers.
+    if(event.keyCode === 13|| event.which === 13){
+       
     }
-}
 
+})
+  
     
 })(budgetController, UIController);
