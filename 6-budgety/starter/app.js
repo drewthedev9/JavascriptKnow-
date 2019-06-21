699f1d7cbb397bed73957eb4e8e2d5ef681cbb47
Andrew Ormond <drewthedev9@gmail.com>
2019-06-21 16:41:46 -0400

    finsihed the UI and the whole app

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -220,6 +220,12 @@ const UIController = (() =>{
         return (type === 'exp' ? sign = '-' :sign = '+') + '' + int + '.' + dec;
     };
 
+     var nodeListForEach = function(list, callback){
+            for (var i = 0; i < list.length; i++){
+                callback(list[i],i);
+            }
+        };
+
 return {
 
     // Function when someone hits teh input key.
@@ -312,11 +318,7 @@ return {
 
         var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-        var nodeListForEach = function(list, callback){
-            for (var i = 0; i < list.length; i++){
-                callback(list[i],i);
-            }
-        };
+       
 
         nodeListForEach(fields, function(current, index){
 
@@ -342,6 +344,18 @@ return {
         document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
     },
     
+    changedType: function(){
+        var fields = document.querySelectorAll(
+            DOMstrings.inputType + ',' +
+            DOMstrings.inputDescription + ',' +
+            DOMstrings.inputValue);
+        
+        nodeListForEach(fields, function(cur){
+            cur.classList.toggle('red-focus');
+        });
+
+        document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+    },
 
     // expose DOMstrings to the public.
     getDOMstrings: function(){
@@ -370,6 +384,8 @@ var controller = (function(budgetCtrl, UICtrl){
 
     // selects he Dom element and adds click function with the function we make ctrlDeleteItem.
     document.querySelector(DOM.container).addEventListener('click',ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 };
 
 var updateBudget = function (){
